2839a957f89898d66a30fb63a675200d0d56d1e8
Justin Koe <100324413+justyoyo123@users.noreply.github.com>
2022-05-13 18:24:27 -0400

    Add files via upload

---
+++ Final_Project_Modules/br_and_confirm.v
@@ -0,0 +1,12 @@
+//Programmer: Diego Toribio
+module br_and_confirm(br, confirm, pc_sel);
+    input br;
+    input confirm;
+    output reg pc_sel;
+
+    always@(*)
+    begin
+        pc_sel = br & confirm;
+    end
+endmodule
+
